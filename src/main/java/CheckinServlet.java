0bffb83ff2e9a00bd2fa11202845ee55ce84c430
Sandeep Parikh <sandeep@clusterbeep.org>
2012-04-14 14:55:41 -0500

    indent fixes

--- src/main/java/CheckinServlet.java
+++ src/main/java/CheckinServlet.java
@@ -18,84 +18,84 @@ import com.mongodb.ReadPreference;
 
 public class CheckinServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Mongo conn = null;
-	private DB db = null;
-	private DBCollection checkins = null;
+    private Mongo conn = null;
+    private DB db = null;
+    private DBCollection checkins = null;
 
-	@Override
-	public void init() throws ServletException {
-		try {
-			conn = new Mongo("127.0.0.1", 27017);
-			db = conn.getDB("simplecheckin");
-			//db.setReadPreference(ReadPreference.SECONDARY);
+    @Override
+    public void init() throws ServletException {
+        try {
+            conn = new Mongo("127.0.0.1", 27017);
+            db = conn.getDB("simplecheckin");
+            //db.setReadPreference(ReadPreference.SECONDARY);
 
-			//if (db.authenticate("admin", "aPYuQaBURFTu".toCharArray())) {
-			//	throw new MongoException("unable to authenticate");
-			//}
+            //if (db.authenticate("admin", "aPYuQaBURFTu".toCharArray())) {
+            //  throw new MongoException("unable to authenticate");
+            //}
 
-			checkins = db.getCollection("checkins");
-			BasicDBObject geoIndex = new BasicDBObject("location", "2d");
-			checkins.ensureIndex(geoIndex);
-		} catch (UnknownHostException e) {
-			System.err.println(e.getMessage());
-			e.printStackTrace();
-		} catch (MongoException e) {
-			System.err.println(e.getMessage());
-			e.printStackTrace();
-		}
-	}
+            checkins = db.getCollection("checkins");
+            BasicDBObject geoIndex = new BasicDBObject("location", "2d");
+            checkins.ensureIndex(geoIndex);
+        } catch (UnknownHostException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        } catch (MongoException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
-	@SuppressWarnings("static-access")
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-		double x = 0.0;
-		double y = 0.0;
+    @SuppressWarnings("static-access")
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        double x = 0.0;
+        double y = 0.0;
 
-		try {
-			x = Double.valueOf(request.getParameter("x")).doubleValue();
-			y = Double.valueOf(request.getParameter("y")).doubleValue();
-		} catch (NullPointerException e) {
-			response.sendError(response.SC_BAD_REQUEST,
-					"missing arguments (double x, double y)");
-		}
-		BasicDBObject query = new BasicDBObject("location", new BasicDBObject(
-				"$near", new double[] { x, y }));
-		List<DBObject> results = checkins.find(query).toArray();
+        try {
+            x = Double.valueOf(request.getParameter("x")).doubleValue();
+            y = Double.valueOf(request.getParameter("y")).doubleValue();
+        } catch (NullPointerException e) {
+            response.sendError(response.SC_BAD_REQUEST,
+                    "missing arguments (double x, double y)");
+        }
+        BasicDBObject query = new BasicDBObject("location", new BasicDBObject(
+                "$near", new double[] { x, y }));
+        List<DBObject> results = checkins.find(query).toArray();
 
-		response.setContentType("text/plain");
-		PrintWriter out = response.getWriter();
-		for (DBObject r : results) {
-			out.println(r.toString());
-		}
-		out.close();
-	}
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        for (DBObject r : results) {
+            out.println(r.toString());
+        }
+        out.close();
+    }
 
-	@SuppressWarnings("static-access")
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-		double x = 0.0;
-		double y = 0.0;
-		String comment = null;
+    @SuppressWarnings("static-access")
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        double x = 0.0;
+        double y = 0.0;
+        String comment = null;
 
-		try {
-			x = Double.valueOf(request.getParameter("x")).doubleValue();
-			y = Double.valueOf(request.getParameter("y")).doubleValue();
-			comment = request.getParameter("comment");
-			if (comment == null) {
-				throw new NullPointerException();
-			}
-		} catch (NullPointerException e) {
-			response.sendError(response.SC_BAD_REQUEST,
-					"missing arguments (double x, double y, string comment)");
-		}
+        try {
+            x = Double.valueOf(request.getParameter("x")).doubleValue();
+            y = Double.valueOf(request.getParameter("y")).doubleValue();
+            comment = request.getParameter("comment");
+            if (comment == null) {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            response.sendError(response.SC_BAD_REQUEST,
+                    "missing arguments (double x, double y, string comment)");
+        }
 
-		BasicDBObject doc = new BasicDBObject();
-		doc.put("comment", comment);
-		doc.put("location", new double[] { x, y });
-		checkins.insert(doc);
+        BasicDBObject doc = new BasicDBObject();
+        doc.put("comment", comment);
+        doc.put("location", new double[] { x, y });
+        checkins.insert(doc);
 
-		response.setStatus(response.SC_OK);
-	}
+        response.setStatus(response.SC_OK);
+    }
 }
